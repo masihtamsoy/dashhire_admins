f4b300b1af67cfefab210c97972f4b0d480f19fd
masihtamsoy <masihtamsoy@gmail.com>
2021-11-30 20:04:04 +0530

    Initial commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+/// TODO: update with your custom SCHEME and HOSTNAME
+const myAuthRedirectUri = 'io.supabase.flutterdemo://login-callback';
+
+/// TODO: Add your SUPABASE_URL / SUPABASE_KEY here
+const supabaseUrl = 'https://llmgbaqnpnyotdvatiyc.supabase.co';
+const supabaseAnnonKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNDI5NDU2OCwiZXhwIjoxOTQ5ODcwNTY4fQ.26pposp1lLkzEowJ3qVVoSAfZAFKRJO9hzPLYKbp9yY';
