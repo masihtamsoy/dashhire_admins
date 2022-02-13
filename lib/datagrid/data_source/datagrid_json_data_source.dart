34f34360593381776c9fa40f8f28837f6431a93e
masihtamsoy <masihtamsoy@gmail.com>
2022-02-14 00:44:19 +0530

    save to Excel: works for web

--- lib/datagrid/data_source/datagrid_json_data_source.dart
+++ lib/datagrid/data_source/datagrid_json_data_source.dart
@@ -1,17 +1,28 @@
 /// Dart import
 import 'dart:convert';
+import 'dart:core';
 
 /// Packages import
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:supabase/supabase.dart' as supa;
 import '../../common/constant.dart';
 
 /// Local import
 import '../data_source/listing_datagridsrouce.dart';
 
+// // Platform specific import
+// import '../../common/export/save_file_mobile.dart'
+//     if (dart.library.html) '../../common/export/save_file_web.dart' as helper;
+import '../../common/export/save_file_web.dart' as helper;
+
 /// DataGrid import
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import 'package:syncfusion_flutter_datagrid_export/export.dart';
+import 'package:syncfusion_flutter_xlsio/xlsio.dart'
+    hide Alignment, Column, Row, Border;
 
 /// Renders column type data grid
 class JsonDataSourceDataGrid extends StatefulWidget {
@@ -101,6 +112,18 @@ class _JsonDataSourceDataGridState extends State {
     jsonDataGridSource = ListingDataGridSource('JSON');
   }
 
+  Future<void> _exportDataGridToExcel() async {
+    final Workbook workbook = _key.currentState!.exportToExcelWorkbook();
+
+    final List<int> bytes = workbook.saveAsStream();
+    workbook.dispose();
+
+    await helper.saveAndLaunchFile(bytes, 'DataGrid.xlsx');
+  }
+
+  final _formKey = GlobalKey<FormBuilderState>();
+  final GlobalKey<SfDataGridState> _key = GlobalKey<SfDataGridState>();
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -118,25 +141,71 @@ class _JsonDataSourceDataGridState extends State {
               : Scaffold(
                   body: Column(
                     children: [
-                      Row(
-                        children: [
-                          Container(
-                              width: 100.0,
-                              child: TextField(
-                                  style: TextStyle(
-                                      // fontSize: 40.0,
-                                      // height: 2.0,
-                                      color: Colors.black))),
-                          TextButton(
-                              child: const Text('Filter'),
-                              onPressed: () {
-                                // _employees.add(Employee(10011, 'Steve', 'Designer', 15000));
-                                // _employeeDataSource.buildDataGridRows();
-                                // _employeeDataSource.updateDataGridSource();
-                              }),
-                        ],
+                      SizedBox(
+                        height: 40.0,
+                        width: 150.0,
+                        child: MaterialButton(
+                            color: Colors.blue,
+                            child: const Center(
+                                child: Text(
+                              'Export to Excel',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                            onPressed: _exportDataGridToExcel),
                       ),
+                      // FormBuilder(
+                      //     key: _formKey,
+                      //     child: Row(
+                      //       children: [
+                      //         Container(
+                      //           width: 100,
+                      //           child: FormBuilderTextField(
+                      //             name: 'ctc',
+                      //             decoration: InputDecoration(
+                      //               labelText: 'CTC',
+                      //             ),
+                      //             keyboardType: TextInputType.number,
+                      //           ),
+                      //         ),
+                      //         SizedBox(width: 20),
+                      //         Container(
+                      //           width: 100,
+                      //           child: FormBuilderTextField(
+                      //             name: 'env',
+                      //             decoration: InputDecoration(
+                      //               labelText: 'Environment',
+                      //             ),
+                      //             keyboardType: TextInputType.text,
+                      //           ),
+                      //         ),
+                      //         SizedBox(width: 20),
+                      //         Container(
+                      //           width: 100,
+                      //           child: FormBuilderTextField(
+                      //             name: 'exp',
+                      //             decoration: InputDecoration(
+                      //               labelText: 'Experience',
+                      //             ),
+                      //             keyboardType: TextInputType.number,
+                      //           ),
+                      //         ),
+                      //         SizedBox(width: 20),
+                      //         TextButton(
+                      //             child: const Text('Filter'),
+                      //             onPressed: () {
+                      //               if (_formKey.currentState
+                      //                       ?.saveAndValidate() ??
+                      //                   false) {
+                      //                 print(_formKey.currentState?.fields);
+                      //               }
+                      //               // _employees.add(Employee(10011, 'Steve', 'Designer', 15000));
+                      //               // _employeeDataSource.buildDataGridRows();
+                      //               // _employeeDataSource.updateDataGridSource();
+                      //             }),
+                      //       ],
+                      //     )),
                       SfDataGrid(
+                          key: _key,
                           // gridLinesVisibility: GridLinesVisibility.both,
                           // headerGridLinesVisibility: GridLinesVisibility.both,
                           source: jsonDataGridSource,
